083caab56a4ebfa0061c3471f38f02347e3e2f6b
Oscar Acuña <oscagustavo1961@gmail.com>
2022-07-25 23:23:19 -0500

    importando Hook UseLocation()

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,13 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
+
 // MasterPage
 export const Layout = () => {
+   const location = useLocation();
+
+   // console.log(location);
+
+   const urlActual = location.pathname;
+
    return (
       <div className="md:flex md:min-h-screen">
          <div className="md:w-1/4 bg-blue-900 px-5 py-10">
@@ -9,13 +16,17 @@ export const Layout = () => {
             {/*Utilizar Navlink para ocupar en el hosting  */}
             <nav className="mt-10">
                <Link
-                  className="text-white text-2xl block mt-2 hover:text-blue-300"
+                  className={`${urlActual === "/clientes" ? "text-blue-300" : "text-white"}
+                  text-2xl block mt-2 hover:text-blue-300`}
                   to="/clientes"
                >
                   Persona
                </Link>
                <Link
-                  className="text-white text-2xl block mt-2 hover:text-blue-300"
+                  className={`${
+                     urlActual === "/clientes/nuevo" ? "text-blue-300" : "text-white"
+                  } 
+                  text-2xl block mt-2 hover:text-blue-300`}
                   to="/clientes/nuevo"
                >
                   Nueva Persona
