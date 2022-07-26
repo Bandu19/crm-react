3bfd4ecaa04fc398a59e8b702368a4272b062f67
Oscar Acuña <oscagustavo1961@gmail.com>
2022-07-25 23:03:11 -0500

    Aplicando Tailwind al Layout Principal

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,9 +2,30 @@ import { Outlet } from "react-router-dom";
 // MasterPage
 export const Layout = () => {
    return (
-      <div>
-         <h1>Desde Layout.jsx</h1>
-         <Outlet />
+      <div className="md:flex md:min-h-screen">
+         <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+            <h2 className="text-4xl font-black text-center text-white ">CRUD-Persona</h2>
+
+            {/*Utilizar Navlink para ocupar en el hosting  */}
+            <nav className="mt-10">
+               <a
+                  className="text-white text-2xl block mt-2 hover:text-blue-300"
+                  href="/clientes"
+               >
+                  Persona
+               </a>
+               <a
+                  className="text-white text-2xl block mt-2 hover:text-blue-300"
+                  href="/clientes/nuevo"
+               >
+                  Nueva Persona
+               </a>
+            </nav>
+         </div>
+
+         <div className="md:w-3/4">
+            <Outlet />
+         </div>
       </div>
    );
 };
