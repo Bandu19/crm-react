6776ca45babdf30cf26032406bc29731b11fbff3
Oscar Acuña <oscagustavo1961@gmail.com>
2022-07-27 20:24:47 -0500

    Validando con Yup

---
+++ src/components/Alerta.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Alerta = ({ children }) => {
+   return (
+      <div
+         className="text-center my-4 bg-red-600 text-white 
+                    font-bold p-3 uppercase"
+      >
+         {/* {errors.pnombre} */}
+         {children}
+      </div>
+   );
+};
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { Alerta } from "./Alerta";
 
 export const Formulario = () => {
+   // Utilizando Yup
+   const nuevoClienteShema = Yup.object().shape({
+      pnombre: Yup.string()
+         .min(4, "El Nombre es muy corto")
+         .max(20, "El Nombre es muy largo")
+         .required("El Nombre de la Persona es Obligatorio"),
+   });
+
+   const handleSubmit = (valores) => {
+      // Enviar datos a la API dentro de esta funcion.
+      console.log(valores);
+   };
    return (
       <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto">
          <h1 className="text-gray-600 font-bold text-xl uppercase text-center">
@@ -16,88 +30,105 @@ export const Formulario = () => {
                pedad: "",
                ptelefono: "",
             }}
+            onSubmit={(values) => {
+               handleSubmit(values);
+            }}
+            validationSchema={nuevoClienteShema}
          >
-            {() => (
-               <Form className="mt-5">
-                  {/*Primer Campo  */}
-                  <div className="mb-4">
-                     <label className="text-gray-900" htmlFor="nombre">
-                        Nombre:
-                     </label>
-                     <Field
-                        id="nombre"
-                        type="text"
-                        className="mt-2 block w-full p-3 bg-gray-200"
-                        placeholder="Nombre de la persona"
-                        name="pnombre"
-                     />
-                  </div>
-                  {/*Segundo Campo */}
-                  <div className="mb-4">
-                     <label className="text-gray-900" htmlFor="apellidop">
-                        Apellido paterno:
-                     </label>
-                     <Field
-                        id="apellidop"
-                        type="text"
-                        className="mt-2 block w-full p-3 bg-gray-200"
-                        placeholder="Primer apellido de la persona"
-                        name="papellidop"
-                     />
-                  </div>
+            {/* Errors es parte de la propiedad que damos a la hora de  */}
+            {({ errors, touched }) => {
+               console.log(errors);
+               console.log(touched);
+               return (
+                  <Form className="mt-5">
+                     {/*Primer Campo  */}
+                     <div className="mb-4">
+                        <label className="text-gray-900" htmlFor="nombre">
+                           Nombre:
+                        </label>
+                        <Field
+                           id="nombre"
+                           type="text"
+                           className="mt-2 block w-full p-3 bg-gray-200"
+                           placeholder="Nombre de la persona"
+                           name="pnombre"
+                        />
 
-                  {/*Tercer Campo*/}
+                        {
+                           // Esto se ocupa para darle estilos css/tailwind para los campos obligatorios
+                           errors.pnombre && touched.pnombre ? (
+                              <Alerta>{errors.pnombre} </Alerta>
+                           ) : null
+                        }
+                        {/* <ErrorMessage name="pnombre" />//Este es un ejemplo de utilizar una propiedad de formik */}
+                     </div>
+                     {/*Segundo Campo */}
+                     <div className="mb-4">
+                        <label className="text-gray-900" htmlFor="apellidop">
+                           Apellido paterno:
+                        </label>
+                        <Field
+                           id="apellidop"
+                           type="text"
+                           className="mt-2 block w-full p-3 bg-gray-200"
+                           placeholder="Primer apellido de la persona"
+                           name="papellidop"
+                        />
+                     </div>
 
-                  <div className="mb-4">
-                     <label className="text-gray-900" htmlFor="apellidom">
-                        Apellido materno:
-                     </label>
-                     <Field
-                        id="apellidom"
-                        type="text"
-                        className="mt-2 block w-full p-3 bg-gray-200"
-                        placeholder="Segundo apellido de la persona"
-                        name="papellidom"
+                     {/*Tercer Campo*/}
+
+                     <div className="mb-4">
+                        <label className="text-gray-900" htmlFor="apellidom">
+                           Apellido materno:
+                        </label>
+                        <Field
+                           id="apellidom"
+                           type="text"
+                           className="mt-2 block w-full p-3 bg-gray-200"
+                           placeholder="Segundo apellido de la persona"
+                           name="papellidom"
+                        />
+                     </div>
+                     {/*Cuarto Campo*/}
+
+                     <div className="mb-4">
+                        <label className="text-gray-900" htmlFor="edad">
+                           Edad:
+                        </label>
+                        <Field
+                           id="edad"
+                           type="number"
+                           className="mt-2 block w-full p-3 bg-gray-200"
+                           placeholder="Edad de la persona"
+                           min="0"
+                           max="100"
+                           name="pedad"
+                        />
+                     </div>
+                     {/*Quinto Campo*/}
+
+                     <div className="mb-4">
+                        <label className="text-gray-900" htmlFor="telefono">
+                           Telefono:
+                        </label>
+                        <Field
+                           id="telefono"
+                           type="tel"
+                           className="mt-2 block w-full p-3 bg-gray-200"
+                           placeholder="Numero de Telefono de la persona"
+                           name="ptelefono"
+                        />
+                     </div>
+
+                     <input
+                        type="submit"
+                        value="Agregar Cliente"
+                        className="mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg"
                      />
-                  </div>
-                  {/*Cuarto Campo*/}
-
-                  <div className="mb-4">
-                     <label className="text-gray-900" htmlFor="edad">
-                        Edad:
-                     </label>
-                     <Field
-                        id="edad"
-                        type="number"
-                        className="mt-2 block w-full p-3 bg-gray-200"
-                        placeholder="Edad de la persona"
-                        min="0"
-                        max="100"
-                        name="pedad"
-                     />
-                  </div>
-                  {/*Quinto Campo*/}
-
-                  <div className="mb-4">
-                     <label className="text-gray-900" htmlFor="telefono">
-                        Telefono:
-                     </label>
-                     <Field
-                        id="telefono"
-                        type="tel"
-                        className="mt-2 block w-full p-3 bg-gray-200"
-                        placeholder="Numero de Telefono de la persona"
-                        name="ptelefono"
-                     />
-                  </div>
-
-                  <input
-                     type="submit"
-                     value="Agregar Cliente"
-                     className="mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg"
-                  />
-               </Form>
-            )}
+                  </Form>
+               );
+            }}
          </Formik>
       </div>
    );
